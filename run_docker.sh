80d62327f8c2f2af501aee5bb5bad77f0d8251b9
Ramil Safin <ramilsafnab1996@gmail.com>
2021-12-25 11:24:37 +0300

    Init

---
+++ run_docker.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Display: ${DISPLAY}"
+
+echo "Allow all local X11 connections"
+
+xhost +local:docker
+
+echo "Running ORB_SLAM2 ROS kinetic Docker image ..."
+
+docker run -it --privileged --name ros-kinetic-orb-slam2 --net=host \
+	-e QT_X11_NO_MITSHM=1 -e DISPLAY=$DISPLAY \
+	--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+        --group-add video \
+        --device=/dev/dri:/dev/dri \
+	lirs/ros-kinetic-desktop:orb-slam2
